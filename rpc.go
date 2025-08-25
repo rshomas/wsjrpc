160d4ac314f16c90a6cc2b2e85b88a167f5a5950
Roman Shomas <rshomas@gmail.com>
2025-08-26 03:11:22 +0700

    Types for JSON-RPC

---
+++ rpc.go
@@ -0,0 +1,22 @@
+package wsjrpc
+
+import "encoding/json"
+
+type Request struct {
+	JSONRPC string          `json:"jsonrpc"`
+	Method  string          `json:"method"`
+	Params  json.RawMessage `json:"params,omitempty"`
+	ID      any             `json:"id,omitempty"`
+}
+
+type Response struct {
+	JSONRPC string `json:"jsonrpc"`
+	Result  any    `json:"result,omitempty"`
+	Error   *Error `json:"error,omitempty"`
+	ID      any    `json:"id,omitempty"`
+}
+
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
